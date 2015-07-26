206dd88cb279533a6656aed11183a91e155178d3
Justin Hearn <justin@hearn.me>
2015-07-26 14:39:53 -0700

    added user route

--- zeen_xyz.py
+++ zeen_xyz.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return 'This is zeen.xyz'
+    return '<h1>This is zeen.xyz</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return "greetings %s" % name
 
 if __name__ == '__main__':
-    app.run( debug=True )
+    manager.run()
