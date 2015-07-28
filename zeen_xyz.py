032fd201539303bcd1e6c36fe01e14d8d42f70fb
Justin Hearn <justin@hearn.me>
2015-07-28 14:00:50 -0700

    added flash message for name change

--- zeen_xyz.py
+++ zeen_xyz.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -24,10 +24,14 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('You have changed your name.')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           current_time=datetime.utcnow())
+    return render_template('index.html', name=session.get('name'),
+                           form=form, current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
