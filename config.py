758f8e58a6a01e06a29f6f5bea9c5b71e7f5f553
Justin Hearn <justin@hearn.me>
2015-07-28 19:51:05 -0700

    centralized configuration

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('ZEEN_SECRET_KEY') or "zeen.xyz Default Secret Key"
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    ZEEN_MAIL_SUBJECT_PREFIX = '[zeen]'
+    ZEEN_MAIL_SENDER = 'zeen Admin <zeen@hearn.me>'
+    ZEEN_ADMIN = os.environ.get('ZEEN_ADMIN') or 'zeen@hearn.me'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'email-smtp.us-west-2.amazonaws.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
