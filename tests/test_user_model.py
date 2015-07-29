2474f924f7035665d9d4fc93b4cd1f24cdbe6c91
Justin Hearn <justin@hearn.me>
2015-07-29 00:18:10 -0700

    confirmation token unit tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,21 @@
 import unittest
+import time
+from app import create_app, db
 from app.models import User
 
+
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
     def test_password_setter(self):
         user = User(password='Test.123')
         self.assertTrue(user.password_hash is not None)
@@ -17,7 +31,31 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(user.verify_password('Wrong_password'))
 
     def test_password_salts_are_random(self):
-        user = User(password='password')
+        user1 = User(password='password')
         user2 = User(password='password')
-        self.assertTrue(user.password_hash != user2.password_hash)
+        self.assertTrue(user1.password_hash != user2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        user = User(password='Test.123')
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        self.assertTrue(user.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        user1 = User(password='Test.123')
+        user2 = User(password='456-Test')
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+        token = user1.generate_confirmation_token()
+        self.assertFalse(user2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        user = User(password='Test.123')
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token(1)
+        time.sleep(2)
+        self.assertFalse(user.confirm(token))
 
