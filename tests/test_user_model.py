d64896b0fe20be9ac8e143bec5e1a707dfd49acc
Justin Hearn <justin@hearn.me>
2015-07-28 22:32:11 -0700

    corrected logic error in password salt unit test

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -17,7 +17,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(user.verify_password('Wrong_password'))
 
     def test_password_salts_are_random(self):
-        user = User(password='Test.123')
-        user2 = User(password='321.Test')
+        user = User(password='password')
+        user2 = User(password='password')
         self.assertTrue(user.password_hash != user2.password_hash)
 
