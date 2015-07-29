d803f1522d1ded88f2c518b88e6de993264ff20f
Justin Hearn <justin@hearn.me>
2015-07-28 22:02:56 -0700

    added command to run unit tests

--- manage.py
+++ manage.py
@@ -18,5 +18,13 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
+@manager.command
+def test():
+    """Runs the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == '__main__':
     manager.run()
