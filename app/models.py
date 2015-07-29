792829cd530eed3f51d6e20c5631f81f98e96e94
Justin Hearn <justin@hearn.me>
2015-07-29 12:29:44 -0700

    added function to securely reset forgotten password

--- app/models.py
+++ app/models.py
@@ -52,6 +52,22 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def generate_reset_token(self, expiration=3600):
+        serializer = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return serializer.dumps({'reset': self.id})
+
+    def reset_password(self, token, new_password):
+        serializer = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = serializer.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
+
 
     def __repr__(self):
         return '<User %r>' % self.username
