b551b2996e2ba126f0ca0da795ac3c21a0d6a673
Justin Hearn <justin@hearn.me>
2015-07-29 23:00:40 -0700

    follow helper methods, db migration

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Post, User, Role, Permission
+from app.models import Follow, Post, User, Role, Permission
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -12,7 +12,8 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission,
+                Post=Post, Follow=Follow)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
